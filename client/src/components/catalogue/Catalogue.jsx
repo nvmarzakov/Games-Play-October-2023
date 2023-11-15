2099ecd1aae113f766a105067ea26e3406f122e8
Nikolay Marzakov <114495254+nvmarzakov@users.noreply.github.com>
2023-11-15 10:25:21 +0200

    Show dynamic game list

--- client/src/components/catalogue/Catalogue.jsx
+++ client/src/components/catalogue/Catalogue.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 
 import * as gameService from '../../services/gameService'
+import GameListItem from "../create-game/game-list-item/GameListItem";
 
 export default function Catalogue() {
     const [games, setGames] = useState([]);
@@ -10,43 +11,20 @@ export default function Catalogue() {
             .then(result => setGames(result));
         
     }, []);
-    
 
-    console.log(games);
-    
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
-            {/* <!-- Display div: with information about every game (if any) --> */}
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>Cover Fire</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
+             {/* <!-- Display div: with information about every game (if any) -->  */}
+            
+            {games.map(game => (
+                <GameListItem key={game._id} {...game} />
+            ))}
 
-            </div>
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>Zombie lang</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
-
-            </div>
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>MineCraft</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
-            </div>
-
-            {/* <!-- Display paragraph: If there is no games  --> */}
-            <h3 className="no-articles">No articles yet</h3>
+            {games.length === 0 && (
+                <h3 className="no-articles">No articles yet</h3>
+            )}
+         
         </section>
     )
 } 
