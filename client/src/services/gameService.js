f3e198a77d984429d7e3d2212c926b773b49b312
Nikolay Marzakov <114495254+nvmarzakov@users.noreply.github.com>
2023-11-15 10:43:50 +0200

    Modify requester

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -1,26 +1,32 @@
-import { request } from "../lib/request";
+import * as request from "../lib/request";
 
 const baseUrl = 'http://localhost:3030/jsonstore/games'
 
 export const getAll = async () => {
-    const result = await request('GET', baseUrl);
-
-    console.log(result);
+    const result = await request.get(baseUrl);
 
     return Object.values(result);
 }
 
 export const create = async (gameData) => {
-    const response = await fetch(baseUrl, { 
-        method: 'POST',
-        headers: {
-            'content-type': 'aplication/json'
-        },
-        body: JSON.stringify(gameData)
+    const result = await request.post(baseUrl, gameData);
 
-    });
+    // const response = await fetch(baseUrl, { 
+    //     method: 'POST',
+    //     headers: {
+    //         'content-type': 'aplication/json'
+    //     },
+    //     body: JSON.stringify(gameData)
 
-    const result = await response.json();
+    // });
+
+    // const result = await response.json();
 
     return result; 
 }
+
+export const get = request.bind(null, 'GET'); 
+export const post = request.bind(null, 'POST'); 
+export const put = request.bind(null, 'PUT'); 
+export const remove = request.bind(null, 'DELETE'); 
+export const patch = request.bind(null, 'PATCH'); 
